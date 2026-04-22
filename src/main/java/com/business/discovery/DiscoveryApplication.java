7d8e224f54119f3730dd4dc5adcf990406ea03d3
yashdahat <yashdahat16@gmail.com>
2026-04-22 09:56:26 +0530

    Phase 1: Removing business retrival logic.

--- src/main/java/com/business/discovery/DiscoveryApplication.java
+++ src/main/java/com/business/discovery/DiscoveryApplication.java
@@ -1,11 +1,7 @@
 package com.business.discovery;
 
-import com.business.discovery.services.googleMapsScrapper.GoogleMapsScraperService;
-import com.business.discovery.model.Business;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
 
 @SpringBootApplication
 public class DiscoveryApplication {
